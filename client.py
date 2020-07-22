493464ac69412040f80e0af483a46c4835a2f33f
Pasha Kopka <pashakopka216@gmail.com>
2020-07-22 11:54:49 +0300

    receive files class version

--- client.py
+++ client.py
@@ -12,18 +12,24 @@ class Client(QWidget):
         self.resize(200, 200)
 
     def send_file_server(self):
-        self.socket = socket.socket()
-        self.socket.connect(('localhost', 9090))
-
+        self.connect_server()
         self.sf = self.socket.fileno()
         self.lf = open('proba.blend', 'rb')
 
         self.socket.sendfile(self.lf)
 
+    def connect_server(self):
+        self.socket = socket.socket()
+        self.socket.connect(('localhost', 9090))
+
+    def close_connection(self):
+        self.socket.close()
+
 
 app = QApplication(sys.argv)
 demo = Client()
 demo.send_file_server()
+demo.close_connection()
 demo.show()
 
 sys.exit(app.exec_())
--- server.py
+++ server.py
@@ -1,28 +1,35 @@
 import socket
 
-server_socket = socket.socket()
 
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+class Server:
 
-server_socket.bind(('localhost', 9090))
-server_socket.listen(1)
-conn, addr = server_socket.accept()
+    def __init__(self):
+        self.connect_client()
+        self.input_blend_file = open('data/proba.blend', 'wb')
+        self.buffer = memoryview(bytearray(1024 * 1024 * 10))
+        self.num_bytes = 1
+        self.receive_file()
 
-pp = open('data/proba.blend', 'wb')
+    def receive_file(self):
+        while self.num_bytes:
+            self.toread = 1024 * 1024 * 10
+            self.view = self.buffer[:]
+            while self.toread:
+                self.num_bytes = self.conn.recv_into(self.view, self.toread)
+                self.view = self.view[self.num_bytes:]
+                self.toread -= self.num_bytes
+                if self.num_bytes == 0:
+                    self.buffer = self.buffer[:-self.toread]
+                    break
 
-buf = memoryview(bytearray(1024 * 1024 * 10))
-nbytes = 1
+            self.input_blend_file.write(self.buffer)
 
-while nbytes:
-    toread = 1024 * 1024 * 10
-    view = buf[:]
-    while toread:
-        nbytes = conn.recv_into(view, toread)
-        view = view[nbytes:]
-        toread -= nbytes
-        if nbytes == 0:
-            buf = buf[:-toread]
-            break
+    def connect_client(self):
+        self.socket = socket.socket()
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(('localhost', 9090))
+        self.socket.listen(1)
+        self.conn, self.addr = self.socket.accept()
 
-    pp.write(buf)
-    print('.', end='')
+
+server = Server()
